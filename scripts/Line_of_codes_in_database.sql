2f0aa8d72784f3396f028043ef2e5556fc1393d8
Sudipta Paul <sudipta116@gmail.com>
2022-11-28 19:51:52 +1100

    Add database line of codes script

---
+++ scripts/Line_of_codes_in_database.sql
@@ -0,0 +1,14 @@
+SELECT DB_NAME(DB_ID()) AS DBName,
+       ObjectType,
+       COUNT(*)         AS Object_Count,
+       SUM(LinesOfCode) AS LinesOfCode
+FROM (SELECT s.type AS ObjectType,
+             LEN(definition)- LEN(REPLACE(definition,CHAR(10),'')) AS LinesOfCode,
+             OBJECT_NAME(a.object_id) AS NameOfObject
+             FROM sys.all_sql_modules AS a
+             INNER JOIN sysobjects AS s
+             ON a.object_id = s.id
+             -- AND xtype IN('TR', 'P', 'FN', 'IF', 'TF', 'V')
+             WHERE OBJECTPROPERTY(a.object_id,'IsMSShipped') = 0
+) SubQuery
+GROUP BY ObjectType;
